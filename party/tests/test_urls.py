8f760758fa15f54c13e5205f9a2e354096f3cace
abWasHere <audrey.belson@gmail.com>
2021-02-19 14:03:26 +0100

    work on party test_urls

--- party/tests/test_urls.py
+++ party/tests/test_urls.py
@@ -1,15 +1,27 @@
 from django.test import TestCase, Client
-
+from django.urls import reverse, resolve
 
 from party.urls import get_post_parties_url, get_venues_url
+
+# initialize the APIClient app
+client = Client()
 
 
 class TestUrls(TestCase):
     def setUp(self):
+
         pass
 
-    def test_party_list_url(self):
-        pass
+    def test_get_post_parties_url(self):
+        """
+        GET ALL PARTIES
+        """
+        # get api response
+        url = reverse('get_post_parties_url')
+        response = client.get(url)
+
+        # get data from db
+
 
     def test_venue_list_url(self):
-        pass
+        pass    
